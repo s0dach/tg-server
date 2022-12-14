099689187676e2c2b49a4901704f0d76755eaefb
Nikita Sidukov <nsidukov@icloud.com>
2022-12-14 17:37:45 +0300

    2.1.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { Telegraf } = require("telegraf");
 const fs = require("fs");
 
 const token = "5960420624:AAEvKvDBpDv5u3aSG2_3jcLULzkZq85aKkA";
-// const webAppUrl = "https://coruscating-cactus-88b025.netlify.app/";
 
 const bot = new Telegraf(token);
+const message = "clear,";
 
 bot.on("message", async (ctx) => {
   const text = ctx.message.text;
@@ -34,86 +34,97 @@ bot.on("message", async (ctx) => {
       ctx.from.username === "s0dach" ||
       ctx.from.username === "SadovoyDmitry")
   ) {
-    await bot.telegram.sendMessage(chatId, "Вы завершили сессию для всех.");
-    fs.unlink("../web-materials/src/lectUsersId.txt", (err) => {
+    bot.telegram.sendMessage(chatId, "Вы завершили сессию для всех.");
+    fs.unlink("../web-materials/src/lectUsersId.txt", function (err) {
       if (err) throw err;
       console.log("all users clear");
     });
-    fs.appendFile("../web-materials/src/lectUsersId.txt", "clear,", (err) => {
-      if (err) throw err;
-      console.log("add new flow");
-    });
+    fs.appendFile(
+      "../web-materials/src/lectUsersId.txt",
+      message,
+      function (err) {
+        if (err) return console.log(err);
+        console.log("okay add clear");
+      }
+    );
   }
-  // //   else {
-  // //     await bot.sendMessage(chatId, "Нет доступа.");
-  // //   }
-
-  // // Панель админа
-  // if (
-  //   text === "/startadmin" &&
-  //   (ctx.from.username === "astrocowboiii" ||
-  //     ctx.from.username === "s0dach" ||
-  //     ctx.from.username === "SadovoyDmitry")
-  // ) {
-  //   await bot.sendMessage(
-  //     chatId,
-  //     "Ниже появится кнопка для перехода в админ раздел.",
-  //     {
-  //       reply_markup: {
-  //         keyboard: [
-  //           [{ text: "Панель администратора", web_app: { url: webAppUrl } }],
-  //         ],
-  //       },
-  //     }
-  //   );
-  // }
-  // //   else {
-  // //     await bot.sendMessage(chatId, "Нет доступа.");
-  // //   }
-
+  // Работаем с документами
   // if (ctx.document) {
   //   usersId.forEach((userId) => {
   //     bot.sendDocument(userId, ctx.document.file_id);
   //   });
   // }
-
-  // if (ctx?.web_app_data?.data) {
-  //   try {
-  //     const data = JSON.parse(ctx?.web_app_data?.data);
-  //     const text = data.text;
-  //     console.log(text);
-  //     if (text) {
-  //       //Забираем ссылку с строки
-  // const links = text.match(/https:\/\/[^\s\Z]+/i);
-  //       const first_link = links?.[0];
-
-  //       usersId.forEach((userId) => {
-  //         if (first_link !== undefined) {
-  //           // Обрезаем конечный текст с картинкой
-
-  //           const firstFinishText = text.replace("<img src=" + first_link, "");
-  //           const lastFinishText = firstFinishText.replace(
-  //             ">" + first_link,
-  //             ""
-  //           );
-  //           const finishedText = lastFinishText.replace("<span><span>", "");
-
-  //           bot.sendPhoto(userId, first_link, {
-  //             caption: finishedText,
-  //             parse_mode: "Markdown",
-  //           });
-  //         }
-
-  //         if (first_link === undefined) {
-  //           bot.sendMessage(userId, text, {
-  //             parse_mode: "Markdown",
-  //           });
-  //         }
-  //       });
-  //     }
-  //   } catch (e) {
-  //     console.warn(e);
-  //   }
-  // }
 });
 bot.launch();
+
+// //   else {
+// //     await bot.sendMessage(chatId, "Нет доступа.");
+// //   }
+
+// // Панель админа
+// if (
+//   text === "/startadmin" &&
+//   (ctx.from.username === "astrocowboiii" ||
+//     ctx.from.username === "s0dach" ||
+//     ctx.from.username === "SadovoyDmitry")
+// ) {
+//   await bot.sendMessage(
+//     chatId,
+//     "Ниже появится кнопка для перехода в админ раздел.",
+//     {
+//       reply_markup: {
+//         keyboard: [
+//           [{ text: "Панель администратора", web-materials_app: { url: web-materialsAppUrl } }],
+//         ],
+//       },
+//     }
+//   );
+// }
+// //   else {
+// //     await bot.sendMessage(chatId, "Нет доступа.");
+// //   }
+
+// if (ctx.document) {
+//   usersId.forEach((userId) => {
+//     bot.sendDocument(userId, ctx.document.file_id);
+//   });
+// }
+
+// if (ctx?.web-materials_app_data?.data) {
+//   try {
+//     const data = JSON.parse(ctx?.web-materials_app_data?.data);
+//     const text = data.text;
+//     console.log(text);
+//     if (text) {
+//       //Забираем ссылку с строки
+// const links = text.match(/https:\/\/[^\s\Z]+/i);
+//       const first_link = links?.[0];
+
+//       usersId.forEach((userId) => {
+//         if (first_link !== undefined) {
+//           // Обрезаем конечный текст с картинкой
+
+//           const firstFinishText = text.replace("<img src=" + first_link, "");
+//           const lastFinishText = firstFinishText.replace(
+//             ">" + first_link,
+//             ""
+//           );
+//           const finishedText = lastFinishText.replace("<span><span>", "");
+
+//           bot.sendPhoto(userId, first_link, {
+//             caption: finishedText,
+//             parse_mode: "Markdown",
+//           });
+//         }
+
+//         if (first_link === undefined) {
+//           bot.sendMessage(userId, text, {
+//             parse_mode: "Markdown",
+//           });
+//         }
+//       });
+//     }
+//   } catch (e) {
+//     console.warn(e);
+//   }
+// }
